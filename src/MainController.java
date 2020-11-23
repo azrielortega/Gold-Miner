53f2ace21504989c066f3d4a44ac7184604fec7a
Azriel <44496680+azrielortega@users.noreply.github.com>
2020-11-23 20:29:20 +0800

    controller code

--- src/MainController.java
+++ src/MainController.java
@@ -1,4 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainController {
+public class MainController implements ActionListener{
+	
+	public MainController() {
+		return;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 	
 }
